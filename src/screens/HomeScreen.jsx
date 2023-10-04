d89b6593fa991fb99c09e04d13d5d543552ba0b5
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-10-04 15:21:41 +0700

    finish build .apk file

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,17 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {dataRecipes} from '../dummy/recipes';
 
 const HomeScreen = () => {
+  const data = dataRecipes.data;
+
   return (
     <View>
       <Text>HomeScreen</Text>
+
+      {data.map((item, index) => (
+        <Text key={index}>{item.title}</Text>
+      ))}
     </View>
   );
 };
