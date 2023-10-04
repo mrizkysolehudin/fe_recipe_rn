c006225b7d153e7995b4a323f49263d42006dc2f
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-10-04 10:08:12 +0700

    add react-native-splash-screen

--- android/app/src/main/java/com/recipe_rn/MainApplication.java
+++ android/app/src/main/java/com/recipe_rn/MainApplication.java
@@ -1,5 +1,6 @@
 package com.recipe_rn;
 
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -25,6 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          
+          new SplashScreenReactPackage(); 
+
           return packages;
         }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
