1bdfdd3a37e6ff11a2994489171741ba08b6a8ec
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-10-05 13:51:18 +0700

    add screens: profile

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,13 +1,114 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Image, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 import BottomTabs from '../components/Global/BottomTabs';
+import {dataUser} from '../dummy/User';
+import {colors} from '../assets/style/colors';
 
 const ProfileScreen = ({route}) => {
   const openTab = route.name || 'Profile';
 
+  const item = dataUser;
+
   return (
     <View style={{position: 'relative', height: '100%'}}>
-      <Text>ProfileScreen</Text>
+      <View
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: colors.yellow,
+          height: 376,
+          position: 'absolute',
+          width: '100%',
+        }}>
+        <Image
+          style={{height: 84, width: 84, borderRadius: 999}}
+          source={{uri: `${item.photo}`}}
+          alt="user"
+        />
+        <Text
+          style={{
+            fontSize: 16,
+            color: 'white',
+            fontWeight: '700',
+            marginTop: 20,
+          }}>
+          {item.name}
+        </Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'white',
+          marginHorizontal: 10,
+          height: 500,
+          position: 'relative',
+          marginTop: 300,
+          borderTopStartRadius: 20,
+          borderTopRightRadius: 20,
+        }}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginLeft: 20,
+            marginTop: 20,
+            width: '100%',
+            position: 'relative',
+          }}>
+          <FeatherIcon name="user" size={24} color={colors.yellow} />
+          <Text style={styles.titleMenuItem}>Edit Profile</Text>
+          <MaterialIcon
+            style={{
+              position: 'absolute',
+              right: 35,
+            }}
+            name="navigate-next"
+            size={25}
+          />
+        </View>
+        <View style={styles.cardMenuItem}>
+          <MaterialIcon name="list" size={24} color={colors.yellow} />
+          <Text style={styles.titleMenuItem}>My Recipes</Text>
+          <MaterialIcon
+            style={{
+              position: 'absolute',
+              right: 35,
+            }}
+            name="navigate-next"
+            size={25}
+          />
+        </View>
+        <View style={styles.cardMenuItem}>
+          <IonIcon name="bookmark-outline" size={24} color={colors.yellow} />
+          <Text style={styles.titleMenuItem}>Saved Recipes</Text>
+          <MaterialIcon
+            style={{
+              position: 'absolute',
+              right: 35,
+            }}
+            name="navigate-next"
+            size={25}
+          />
+        </View>
+        <View style={styles.cardMenuItem}>
+          <AntDesignIcon name="like2" size={24} color={colors.yellow} />
+          <Text style={styles.titleMenuItem}>Liked Recipes</Text>
+          <MaterialIcon
+            style={{
+              position: 'absolute',
+              right: 35,
+            }}
+            name="navigate-next"
+            size={25}
+          />
+        </View>
+      </View>
 
       <BottomTabs openTab={openTab} />
     </View>
@@ -15,3 +116,21 @@ const ProfileScreen = ({route}) => {
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  cardMenuItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginTop: 40,
+    width: '100%',
+    position: 'relative',
+  },
+  titleMenuItem: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: 'rgba(0, 0, 0, 0.70)',
+    marginLeft: 10,
+  },
+});
